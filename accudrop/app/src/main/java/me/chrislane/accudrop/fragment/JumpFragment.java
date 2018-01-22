10d35dc3e52a84d69a42a625dea8401bbf9cc9f5
Chris Lane <cml476@cs.bham.ac.uk>
2018-01-22 13:22:44 +0000

    Fix bug where Jump button would have incorrect state.
    If the user switched to another fragment then the savedInstanceState
    would be null and so no check would be made for carrying out the state change.
    Since we now have background checks, we can see if the LocationService is running
    and infer from that what the jump button state should be.

--- accudrop/app/src/main/java/me/chrislane/accudrop/fragment/JumpFragment.java
+++ accudrop/app/src/main/java/me/chrislane/accudrop/fragment/JumpFragment.java
@@ -1,8 +1,10 @@
 package me.chrislane.accudrop.fragment;
 
 
+import android.app.ActivityManager;
 import android.arch.lifecycle.DefaultLifecycleObserver;
 import android.arch.lifecycle.LifecycleOwner;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -20,6 +22,7 @@ import me.chrislane.accudrop.R;
 import me.chrislane.accudrop.Util;
 import me.chrislane.accudrop.Util.Unit;
 import me.chrislane.accudrop.presenter.JumpPresenter;
+import me.chrislane.accudrop.service.LocationService;
 
 public class JumpFragment extends Fragment implements DefaultLifecycleObserver {
 
@@ -49,11 +52,11 @@ public class JumpFragment extends Fragment implements DefaultLifecycleObserver {
         Button calibrateButton = view.findViewById(R.id.calibrate_button);
         calibrateButton.setOnClickListener(this::onClickCalibrate);
 
+        // Set the jump button toggle state
         ToggleButton jumpButton = view.findViewById(R.id.jump_button);
         jumpButton.setOnCheckedChangeListener(null);
-        if (savedInstanceState != null) {
-            jumpButton.setChecked(savedInstanceState.getBoolean("jumpButton", false));
-        }
+        Log.d(TAG, "LocationService running: " + isServiceRunning(LocationService.class));
+        jumpButton.setChecked(isServiceRunning(LocationService.class));
         jumpButton.setOnCheckedChangeListener(onClickJump());
 
         return view;
@@ -106,5 +109,22 @@ public class JumpFragment extends Fragment implements DefaultLifecycleObserver {
     @Override
     public void onPause(@NonNull LifecycleOwner owner) {
         jumpPresenter.pause();
+    }
+
+    /**
+     * <p>Check if a service is running.</p>
+     * <p>Code taken from <a href="https://stackoverflow.com/a/5921190">a StackOverflow answer.</a></p>
+     *
+     * @param serviceClass The service class to check for an instance of.
+     * @return Whether the service is running.
+     */
+    private boolean isServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getActivity().getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
