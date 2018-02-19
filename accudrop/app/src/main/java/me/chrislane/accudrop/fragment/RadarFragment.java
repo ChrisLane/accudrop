97caf35cadb35d1c3e7c56d23ef53abc00db75f3
Chris Lane <cml476@cs.bham.ac.uk>
2018-02-19 12:22:25 +0000

    Add ability to present many user's positions on the radar view.
    Currently a mess of code with ridiculous static values for things like
    maximum horizontal and vertical distances.

--- accudrop/app/src/main/java/me/chrislane/accudrop/fragment/RadarFragment.java
+++ accudrop/app/src/main/java/me/chrislane/accudrop/fragment/RadarFragment.java
@@ -4,28 +4,37 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.util.Pair;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import me.chrislane.accudrop.R;
+import me.chrislane.accudrop.Util;
 import me.chrislane.accudrop.presenter.RadarPresenter;
 
 public class RadarFragment extends Fragment {
 
+    private static final String TAG = RadarFragment.class.getSimpleName();
     private RadarPresenter presenter;
+    private Radar radar;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        presenter = new RadarPresenter();
+        presenter = new RadarPresenter(this);
     }
 
     @Override
@@ -34,11 +43,78 @@ public class RadarFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_radar, container, false);
         FrameLayout layout = view.findViewById(R.id.radar);
-        layout.addView(new Radar(getContext()));
+        radar = new Radar(getContext());
+        layout.addView(radar);
         return view;
     }
 
-    public static class Radar extends View {
+    public void updateRadarPoints() {
+        radar.invalidate();
+    }
+
+    private List<PointF> getScaledPositions(int width, int height, float shrink) {
+        int maxHDistance = presenter.getMaxHDistance();
+        List<Pair<Float, Float>> positions = presenter.getPositions(); // bearing, distance
+        List<PointF> result = new ArrayList<>();
+
+        for (Pair<Float, Float> position : positions) {
+            if (position.first != null && position.second != null) {
+                // Scale distance to ellipse width
+                double scaledDistance = Util.getScaledValue(position.second, 0, maxHDistance, 0, width / 2f);
+                Log.v(TAG, "Scaled H Distance: " + scaledDistance);
+                // Calculate cartesian coordinate
+                PointF newPoint = calculateCartesian(position.first, scaledDistance);
+                Log.v(TAG, "After Cartesian: " + newPoint);
+                // Apply shrink to stay within ellipse
+                newPoint = applyShrink(newPoint, shrink);
+                Log.v(TAG, "After Shrink: " + newPoint);
+                // Move position to be relative to the centre of the screen
+                newPoint = calculateRelative(newPoint, new PointF(width / 2, height / 2));
+                Log.v(TAG, "After Relative: " + newPoint);
+
+                // Add to the result
+                result.add(newPoint);
+            }
+        }
+
+        return result;
+    }
+
+    private List<Double> getScaledAltitudes(int height) {
+        int maxVDistance = presenter.getMaxVDistance();
+        List<Double> heightDiffs = presenter.getHeightDiffs();
+        List<Double> result = new ArrayList<>();
+
+        for (Double heightDiff : heightDiffs) {
+            // Scale the vertical distance with the screen height
+            double scaledDiff = Util.getScaledValue(heightDiff, 0, maxVDistance, 0, height / 2f);
+            Log.v(TAG, "Scaled V Distance: " + scaledDiff);
+            result.add(scaledDiff);
+        }
+
+        return result;
+    }
+
+    private static PointF applyShrink(PointF point, float shrink) {
+        // TODO: Scale to ellipse, not just height change in ellipse.
+        return new PointF(point.x, point.y * shrink);
+    }
+
+    private static PointF calculateCartesian(Float bearing, double scaledDistance) {
+        double rBearing = Math.toRadians(bearing);
+        float x = (float) (scaledDistance * Math.sin(rBearing));
+        float y = (float) (scaledDistance * Math.cos(rBearing));
+        return new PointF(x, y);
+    }
+
+    private static PointF calculateRelative(PointF point, PointF subject) {
+        float x = subject.x - point.x;
+        float y = subject.y + point.y;
+        return new PointF(x, y);
+    }
+
+    public class Radar extends View {
+        private final String TAG = Radar.class.getSimpleName();
         Paint paint = new Paint();
         RectF oval = new RectF();
 
@@ -49,18 +125,45 @@ public class RadarFragment extends Fragment {
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
+            int width = canvas.getWidth();
+            int height = canvas.getHeight();
             float halfHeight = canvas.getHeight() / 2f;
-            float quarterWidth = canvas.getWidth() / 4f;
+            float halfWidth = width / 2f;
+            float shrink = 0.25f;
+            float heightMove = halfWidth * shrink;
 
             paint.setColor(Color.BLACK);
             paint.setStyle(Paint.Style.STROKE);
             paint.setStrokeWidth(5);
-            oval.set(0, halfHeight - quarterWidth, canvas.getWidth(),
-                    halfHeight + quarterWidth);
+            oval.set(0, halfHeight - heightMove, width, halfHeight + heightMove);
             canvas.drawOval(oval, paint);
 
-            oval.inset(canvas.getWidth() * 0.5f, (canvas.getWidth() / 2f) * 0.5f);
+            oval.inset(halfWidth / 2, heightMove / 2);
             canvas.drawOval(oval, paint);
+
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(Color.BLUE);
+            canvas.drawCircle(halfWidth, halfHeight, (width + height) * 0.003f, paint);
+
+            List<PointF> positions = getScaledPositions(width, height, shrink);
+            List<Double> heightDiffs = getScaledAltitudes(height);
+            paint.setColor(Color.RED);
+            for (int i = 0; i < positions.size(); i++) {
+                PointF position = positions.get(i);
+                Log.v(TAG, "Width: " + width + ", Height: " + height);
+                Log.v(TAG, "Adding Pos: " + position);
+
+                // Draw line from original x,y to height adjusted x,y
+                float newY = (float) (position.y + heightDiffs.get(i));
+                paint.setColor(Color.BLACK);
+                canvas.drawLine(width - position.x, height - position.y,
+                        width - position.x, height - newY, paint);
+
+                // Draw circle
+                paint.setColor(Color.RED);
+                canvas.drawCircle(width - position.x, height - newY,
+                        (width + height) * 0.003f, paint);
+            }
         }
     }
 }
--- accudrop/app/src/main/java/me/chrislane/accudrop/presenter/RadarPresenter.java
+++ accudrop/app/src/main/java/me/chrislane/accudrop/presenter/RadarPresenter.java
@@ -1,13 +1,129 @@
 package me.chrislane.accudrop.presenter;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.support.v4.util.Pair;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import me.chrislane.accudrop.MainActivity;
+import me.chrislane.accudrop.fragment.RadarFragment;
+import me.chrislane.accudrop.task.FetchUsersAndPositionsTask;
+import me.chrislane.accudrop.viewmodel.JumpViewModel;
+
 public class RadarPresenter {
 
-    private void getPosOnRadar() {
-        // Main radar character is at the centre.
-        // Edge of radar is distance MAX from centre
-        // Get bearing and distance of subject from main radar character
-        // Convert distance in bearing to X,Y coordinate on screen, marking position on radar
-        // Move position up a scaled amount to show altitude difference
-        // Draw line from pos before altitude step to pos adjusted for altitude
+    private static final String TAG = RadarPresenter.class.getSimpleName();
+    private final RadarFragment fragment;
+    private JumpViewModel jumpViewModel = null;
+    private UUID subject;
+    private int maxHDistance = 500; // In metres
+    private int maxVDistance = 50; // In metres
+    private List<Location> subjectLocs;
+    private List<Pair<UUID, List<Location>>> guestLocs;
+    private List<Pair<Float, Float>> positions = new ArrayList<>();
+    private List<Double> heightDiffs = new ArrayList<>();
+
+    public RadarPresenter(RadarFragment fragment) {
+        this.fragment = fragment;
+
+        MainActivity main = (MainActivity) fragment.getActivity();
+        if (main != null) {
+            jumpViewModel = ViewModelProviders.of(main).get(JumpViewModel.class);
+        }
+
+        // Set the current user as the subject
+        SharedPreferences settings = jumpViewModel.getApplication()
+                .getSharedPreferences("userInfo", Context.MODE_PRIVATE);
+        String stringUuid = settings.getString("userUUID", "");
+        UUID uuid = UUID.fromString(stringUuid);
+        setSubject(uuid);
+
+        // Get users and positions of the last jump
+        FetchUsersAndPositionsTask.Listener listener = userEntries -> {
+            for (int i = 0; i < userEntries.size(); i++) {
+                Pair<UUID, List<Location>> userEntry = userEntries.get(i);
+                if (userEntry.first.equals(subject)) {
+                    subjectLocs = userEntry.second;
+                    userEntries.remove(i);
+                }
+            }
+
+            // None of the users were the subject.
+            if (subjectLocs == null) {
+                Log.e(TAG, "Subject does not exist in jump data.");
+                return;
+            }
+
+            guestLocs = userEntries;
+            List<Location> locations = gatherLocations();
+            generatePositions(locations);
+        };
+        new FetchUsersAndPositionsTask(listener, jumpViewModel).execute();
+    }
+
+    private List<Location> gatherLocations() {
+        List<Location> result = new ArrayList<>();
+        for (Pair<UUID, List<Location>> userEntry : guestLocs) {
+            // TODO: Get the same entry for relative time for all guests
+            if (userEntry.second.size() > 0) {
+                result.add(userEntry.second.get(25));
+            }
+        }
+        return result;
+    }
+
+    public void setSubject(UUID uuid) {
+        subject = uuid;
+    }
+
+    public void generatePositions(List<Location> locations) {
+        if (subjectLocs.isEmpty() || locations.isEmpty()) {
+            return;
+        }
+        Location subjectLoc = subjectLocs.get(25);
+
+        positions.clear();
+        heightDiffs.clear();
+
+        // Loop over user position arrays
+        for (Location guest : locations) {
+            float hDistanceTo = subjectLoc.distanceTo(guest);
+            double vDistanceTo = guest.getAltitude() - subjectLoc.getAltitude();
+            Log.v(TAG, "Horizontal Distance: " + hDistanceTo);
+            Log.v(TAG, "Vertical Distance: " + vDistanceTo);
+
+            // Check if distance from subject further than maxHDistance
+            if (maxHDistance >= hDistanceTo && maxVDistance >= vDistanceTo) {
+                // Add to list of positions to draw
+                float bearingTo = subjectLoc.bearingTo(guest);
+                Log.v(TAG, "Bearing: " + bearingTo);
+                positions.add(new Pair<>(bearingTo, hDistanceTo));
+                heightDiffs.add(vDistanceTo);
+            }
+        }
+
+        fragment.updateRadarPoints();
+    }
+
+    public List<Pair<Float, Float>> getPositions() {
+        return positions;
+    }
+
+    public List<Double> getHeightDiffs() {
+        return heightDiffs;
+    }
+
+    public int getMaxVDistance() {
+        return maxVDistance;
+    }
+
+    public int getMaxHDistance() {
+        return maxHDistance;
     }
 }
