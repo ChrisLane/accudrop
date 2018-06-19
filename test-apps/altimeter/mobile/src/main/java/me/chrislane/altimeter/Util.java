a6f9311176d37ee960a591127f629f5345bf41ce
Chris Lane <chris@chrislane.com>
2018-06-19 16:30:56 +0100

    Restructure as app only repo

--- test-apps/altimeter/mobile/src/main/java/me/chrislane/altimeter/Util.java
+++
@@ -1,9 +0,0 @@
-package me.chrislane.altimeter;
-
-public class Util {
-    public static float metresToFeet(float metres) {
-        return 3.281f * metres;
-    }
-
-    public enum Unit {METRIC, IMPERIAL}
-}
