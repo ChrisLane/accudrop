0a893d64dc529e6bf41dd34a6af019c00d165481
Chris Lane <chris@chrislane.com>
2019-08-26 14:38:47 +0100

    Move variable assignment out of onCreate

--- app/src/main/java/me/chrislane/accudrop/fragment/PlanFragment.kt
+++ app/src/main/java/me/chrislane/accudrop/fragment/PlanFragment.kt
@@ -1,19 +1,19 @@
 package me.chrislane.accudrop.fragment
 
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.preference.PreferenceManager
-import androidx.fragment.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -32,10 +32,10 @@ import me.chrislane.accudrop.viewmodel.RouteViewModel
 class PlanFragment : Fragment(), LifecycleOwner, OnMapReadyCallback, SharedPreferences.OnSharedPreferenceChangeListener {
     private lateinit var map: GoogleMap
     private lateinit var gnssViewModel: GnssViewModel
-    private lateinit var camPosBuilder: CameraPosition.Builder
     private lateinit var routeViewModel: RouteViewModel
     private lateinit var planPresenter: PlanPresenter
     private lateinit var preferences: SharedPreferences
+    private val camPosBuilder: CameraPosition.Builder = CameraPosition.Builder().zoom(15.5f).bearing(0f).tilt(0f)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,11 +49,6 @@ class PlanFragment : Fragment(), LifecycleOwner, OnMapReadyCallback, SharedPrefe
         }
 
         preferences = PreferenceManager.getDefaultSharedPreferences(context)
-
-        camPosBuilder = CameraPosition.Builder()
-                .zoom(15.5f)
-                .bearing(0f)
-                .tilt(0f)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
